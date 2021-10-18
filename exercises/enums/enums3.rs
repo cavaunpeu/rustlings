6beb262a3f5e3f47e97721d01bfa918643e2e3c2
Will Wolf <will@asapp.com>
2021-10-18 09:32:35 +0200

    * completed hashmap1.rs * completed hashmap2.rs * completed vec1.rs * completed vec2.rs * completed enums1.rs * completed enums2.rs * completed enums3.rs * completed modules1.rs * completed modules2.rs * completed modules3.rs * completed structs3.rs

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Quit,
+    Echo(String),
+    ChangeColor((u8, u8, u8)),
+    Move(Point)
 }
 
 struct Point {
@@ -36,7 +37,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::Quit => self.quit(),
+            Message::Echo(string) => self.echo(string),
+            Message::ChangeColor((r, g, b)) => self.change_color((r, g, b)),
+            Message::Move(point) => self.move_position(point)
+        }
     }
 }
 
